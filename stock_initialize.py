b92b26c2ebd595306df58cc9d80a9d986f770b1a
Akhil Sharma <akhilsharma.off@gmail.com>
2023-09-24 22:48:28 +0530

    Add csv reading ticker column

--- stock_initialize.py
+++ stock_initialize.py
@@ -10,28 +10,6 @@ import csv
 API_Key = config.api_key
 client = RESTClient(api_key=API_Key)
 
-class stock:
-    #Initializing the parameters for the stock preditions
-    ticker: List[str]=input("Give your stock name {eg: Apple:APPL}: ")
-    timespan: str = input("Give size of the time window {eg: day}: ")
-    multiplier: int = input("Give size of the timespan multiplier {eg: 1}: ")
-    start_date: str = input("Give the starting date {format '2023-0X-XX'} : ")
-    end_date: str = input("Give the ending date {format '2023-0X-XX'} : ")
-
-    data_Collection = cast(
-        HTTPResponse,
-        client.get_aggs(
-            ticker,
-            int(multiplier),
-            timespan,
-            start_date,
-            end_date,
-            raw=True
-        ),
-    )
-    data_Processing = json.loads(data_Collection.data)
-    print(data_Processing)
-
 class csv_data_extractor:
 
     # Initialize the variable for the csv path finder
@@ -40,6 +18,7 @@ class csv_data_extractor:
     # Open the CSV file for reading
     csv_file = csv_path  # Replace with your CSV file's path
 
+    # Reading the data inside the ticker column in the csv
     try:
         with open(csv_file, 'r') as file:
             csv_reader = csv.reader(file)
@@ -75,3 +54,45 @@ class csv_data_extractor:
         print(f"File '{csv_file}' not found.")
     except Exception as e:
         print(f"An error occurred: {str(e)}")
+
+    
+
+
+class stock:
+    #Initializing the parameters for the stock preditions
+    ticker: List[str]=input("Give your stock name {eg: Apple:APPL}: ")
+    timespan: str = input("Give size of the time window {eg: day}: ")
+    multiplier: int = input("Give size of the timespan multiplier {eg: 1}: ")
+    start_date: str = input("Give the starting date {format '2023-0X-XX'} : ")
+    end_date: str = input("Give the ending date {format '2023-0X-XX'} : ")
+
+    # Initialize the closing price variable 
+    close_list=[]
+
+    #Checking if it is empty or not
+    if close_list:
+        close_list = []
+
+    # Iterate through each tickers and store the value of the closing date
+    for ticker in csv_data_extractor.ticker_values:
+        aggs = client.get_aggs(
+            ticker,
+            int(multiplier),
+            timespan,
+            start_date,
+            end_date,
+            raw=True
+        )
+        
+        data = json.loads(aggs.data)
+        
+        if "results" in data:
+            raw_data_stock = data["results"]
+            
+            for bar in raw_data_stock:
+                if "c" in bar:
+                    close_list.append(bar["c"])
+
+    # Debugging intialized 
+    #print(close_list)
+    
