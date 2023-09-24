b8569ba563f258b039c97340df0df6f1339e823f
Akhil Sharma <akhilsharma.off@gmail.com>
2023-09-24 22:31:35 +0530

    Intialize the parameters variable for stock pred

--- stock_initialize.py
+++ stock_initialize.py
@@ -10,4 +10,9 @@ API_Key = config.api_key
 client = RESTClient(api_key=API_Key)
 
 class stock:
-    
+    #Initializing the parameters for the stock preditions
+    ticker: List[str]=input("Give your stock name {eg: Apple:APPL}: ")
+    timespan: str = input("Give size of the time window {eg: day}: ")
+    multiplier: int = input("Give size of the timespan multiplier {eg: 1}: ")
+    start_date: str = input("Give the starting date {format '2023-0X-XX'} : ")
+    end_date: str = input("Give the ending date {format '2023-0X-XX'} : ")
