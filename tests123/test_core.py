e314fedc5db5e0e3633793681db793fec2fa1127
Akhil Sharma <akhilsharma.off@gmail.com>
2023-10-03 00:46:43 +0530

    Remove files

--- tests123/test_core.py
+++
@@ -1,7 +0,0 @@
-import pytest
-from stockify.core import StockExtractor
-
-ticker_values='APPL'
-timespan= 'DYX'
-stock_closing_price_list = StockExtractor.ticker_data_collection(ticker_values,'day',1,'2023-08-09')
-
