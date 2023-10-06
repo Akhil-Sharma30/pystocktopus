b9048119dd14d0fbf7ae19eeaf18d541271ed681
Akhil Sharma <akhilsharma.off@gmail.com>
2023-10-06 17:59:23 +0530

    Add config.py file

---
+++ PyStoAnalyzer/config.py
@@ -0,0 +1,12 @@
+"""
+Copyright (c) 2023 Akhil Sharma. All rights reserved.
+
+PyStoAnalyzer.
+"""
+import os
+
+#API Configeration
+api_key = os.environ.get("Stock_API_KEY")
+
+#News API Configeration
+news_api = os.environ.get("news_API_KEY")
